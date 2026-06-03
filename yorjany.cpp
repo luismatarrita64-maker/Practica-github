f55a817a05e80ac072fd3cedb3231508c8d0a55c
yorjany <rodriguezyorjany@gmail.com>
2026-06-02 18:23:54 -0600

    lo subio yorjany

---
+++ yorjany.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    cout << "La suma es: " << num1 + num2 << endl;
+
+    return 0;
+}
